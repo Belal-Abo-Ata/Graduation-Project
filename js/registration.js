65748f792c41316a687f62ccc0bfd855a3d11620
Night-Revan <belalbelalmohamed1234@gmail.com>
2023-03-27 17:47:08 +0200

    Add Commits & Adjust the nav bar

--- js/registration.js
+++ js/registration.js
@@ -4,23 +4,53 @@ const firstForm = document.querySelector(`[data-first-form]`);
 const secondForm = document.querySelector(`[data-second-form]`);
 const firstFormBtn = document.querySelector(`[data-first-form-btn]`);
 const secondFormBtn = document.querySelector(`[data-second-form-btn]`);
+const fullName = document.querySelector(`[data-name]`);
 
 // Event Listeners
 
-firstFormBtn.addEventListener(`click`, e => {
-  e.preventDefault();
-  firstForm.classList.add(`-left-full`);
-  firstForm.classList.remove(`left-0`);
-  secondForm.classList.add(`left-0`);
-  secondForm.classList.remove(`left-full`);
-});
+firstForm.addEventListener(`submit`, formValidate);
 
-secondFormBtn.addEventListener(`click`, e => {
-  e.preventDefault();
-  secondForm.classList.add(`left-full`);
-  secondForm.classList.remove(`left-0`);
-  firstForm.classList.add(`left-0`);
-  firstForm.classList.remove(`-left-full`);
-});
+// firstFormBtn.addEventListener(`click`, e => {
+//   e.preventDefault();
+//   firstForm.classList.add(`-left-full`);
+//   firstForm.classList.remove(`left-0`);
+//   secondForm.classList.add(`left-0`);
+//   secondForm.classList.remove(`left-full`);
+// });
 
-console.log(firstForm, secondForm, firstFormBtn);
+// secondFormBtn.addEventListener(`click`, e => {
+//   e.preventDefault();
+//   secondForm.classList.add(`left-full`);
+//   secondForm.classList.remove(`left-0`);
+//   firstForm.classList.add(`left-0`);
+//   firstForm.classList.remove(`-left-full`);
+// });
+
+// Functions
+
+function formValidate(e) {
+  // const emailValid = emailValidate();
+  // const passwordValid = passwordValidate();
+  // const formValid = emailValid && passwordValid;
+  const formValid = nameValidate();
+  if (!formValid) {
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+  }
+}
+
+function nameValidate() {
+  const value = fullName.value;
+  const regEx = (validRegex = /\b([a-zA-z][-,a-z. ']+[ ]*)+/);
+  const valid = regEx.test(value);
+  if (!valid) {
+    fullname.nextElementSibling.textContent = `Please enter a valid Name`;
+    fullName.classList.add(`border-red-500`);
+  } else {
+    fullName.nextElementSibling.textContent = ``;
+    fullName.classList.remove(`border-red-500`);
+  }
+  return valid;
+}
+console.log(firstForm, secondForm, firstFormBtn, fullName);
